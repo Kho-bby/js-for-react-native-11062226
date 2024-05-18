96ac05019560a7e8867b92a25beeb9d02df2311b
kho_bby <cckonor@st.ug.edu.gh>
2024-05-18 07:36:47 +0000

    Task 3 complete

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,7 +1,7 @@
-
-function processArray(arr) {
-    return arr.map(number =>{
-        if (number % 2 === 0 ){
+//** Task 1 */
+function processArray(numbers) {
+    return numbers.map(number =>{
+        if (numbers % 2 === 0 ){
             return number * number;
         }else{
             return number * 3;
@@ -9,12 +9,14 @@ function processArray(arr) {
     })
 }
 
-function formatArrayStrings(stringsArray, arr){
-    if(stringsArray.length !== arr.length){
+
+//** Task 2*/
+function formatArrayStrings(stringsArray, numbersArray){
+    if(stringsArray.length !== numbersArray.length){
         throw new Error;
     }
     return stringsArray.map((str, index) => {
-        const number = arr[index];
+        const number = numbersArray[index];
         if (number % 2 === 0) {
             return str.toUpperCase();
         }else{
